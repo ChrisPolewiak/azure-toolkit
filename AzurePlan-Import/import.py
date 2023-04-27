03c8a5a23c213e86161d3fbd31d0562a8c71b65c
Chris Polewiak <chris@polewiak.pl>
2023-04-27 15:24:59 +0200

    Add http server

--- AzurePlan-Import/import.py
+++ AzurePlan-Import/import.py
@@ -7,27 +7,31 @@
 # V1.00, 2022-07-15 - Initial public version
 # V1.01, 2022-11-21 - Add CSV Support
 # v1.02, 2023-04-12 - Add HTML output and add parser for input arguments
+# v2.00, 2023-04-27 - Remove Excel support, Add HTTP server using Flesk
 
 import sys
 import argparse
 import AzurePlan
 
 parser = argparse.ArgumentParser(prog='import.py',
-                                 description='Import, extract and calculate billing in AzurePlan subscriptions from Excel and CSV files',
+                                 description='Import, extract and calculate billing in AzurePlan subscriptions from CSV files',
                                  epilog='Source: https://github.com/ChrisPolewiak/azure-toolkit/tree/master/AzurePlan-Import',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-i', '--input', required=True, help='input file in CSV or Excel format')
-parser.add_argument('-f', '--format', required=False, help='output format: txt or html', default='txt')
+parser.add_argument('-o', '--output', required=False, help='output format: txt or json', default='txt')
 args = parser.parse_args()
 
 if ( args.input ):
     filepath = args.input
 
-    print (filepath)
     report = AzurePlan.Import( filepath )
     billing = AzurePlan.Calculate( report )
 
-    if ( args.format == 'html' ):
-        AzurePlan.ReportHTML( billing )
+    if ( args.output == 'json' ):
+        report = AzurePlan.ReportJSON( billing )
     else:
-        AzurePlan.ReportTXT( billing )
+        report = AzurePlan.ReportTXT( billing )
+    print("------------------------------------------------------------")
+    print(report)
+    print("------------------------------------------------------------")
+
